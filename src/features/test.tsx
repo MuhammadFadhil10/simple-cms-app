97f692a431179eb6a0aaa76400132d46d6c74ea2
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2023-05-02 15:57:40 +0700

    initial commit

---
+++ src/features/test.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export const TestImportAlias = React.memo(function TestImportAlias() {
+	return <h1>This component imported with path alias import</h1>;
+});
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+import { TestImportAlias } from "@/features";
+
+const App = React.memo(function App() {
+	return (
+		<>	
+			<h1>Hello from home</h1>
+			<TestImportAlias />
+		</>
+	);
+});
+
+export default App;
