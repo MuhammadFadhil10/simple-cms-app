c79163cf8a368978edc18d3aa0b11a2d5831e495
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2023-06-12 22:20:00 +0700

    fix: cleanup

--- src/features/web/components/main/MainHeader.tsx
+++ src/features/web/components/main/MainHeader.tsx
@@ -1,16 +1,11 @@
 import * as React from "react";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import { useMain } from "@/features/web";
 
 export const MainHeader = React.memo(function MainHeader() {
-  const { memoizedCurrentWeb } = useMain();
-
   return (
     <Stack sx={{ backgroundColor: "blue", position: "fixed", width: "100vw" }}>
-      <Typography fontSize={30}>
-        {(memoizedCurrentWeb as any)?.webName} Web Editor
-      </Typography>
+      <Typography fontSize={30}>Web Editor</Typography>
     </Stack>
   );
 });
